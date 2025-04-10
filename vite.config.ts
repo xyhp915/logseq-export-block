9e150d21b12c33289c81e6fa5b3950eb6bd2ad1c
charlie <xyhp915@qq.com>
2025-04-10 11:10:35 +0800

    ci: build dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    sourcemap: true,
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'src/index.ts',
-      output: {
-        entryFileNames: 'index.js',
-        format: 'es',
-        dir: 'dist'
-      }
-    }
-  },
   server: {
     port: 3000,
     strictPort: true
